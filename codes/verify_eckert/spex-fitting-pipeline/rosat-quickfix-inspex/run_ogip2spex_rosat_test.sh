46be83f0d214ab1db3c569356331f95dee89b30d
Yanling Chen <cyl129@163.com>
2023-11-09 09:15:00 +0100

    add --keep-grouping to ogip2spex

--- codes/verify_eckert/spex-fitting-pipeline/rosat-quickfix-inspex/run_ogip2spex_rosat_test.sh
+++ codes/verify_eckert/spex-fitting-pipeline/rosat-quickfix-inspex/run_ogip2spex_rosat_test.sh
@@ -3,5 +3,5 @@ export RMFFILE=rosat_binfix.rsp
 export SPOFILE=rosat.spo
 export RESFILE=rosat.res
 
-ogip2spex --phafile $PHAFILE --rmffile $RMFFILE --spofile $SPOFILE --resfile $RESFILE
+ogip2spex --phafile $PHAFILE --rmffile $RMFFILE --spofile $SPOFILE --resfile $RESFILE --keep-grouping
 # ogip2spex --phafile $PHAFILE --rmffile $RMFFILE --arffile $ARFFILE --spofile $SPOFILE --resfile $RESFILE
