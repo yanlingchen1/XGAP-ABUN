0a0c2ae61b84dff044d17a824f0896c03dfe1dcd
Yanling Chen <cyl129@163.com>
2023-11-20 05:27:44 +0100

    change xspec-fitall.sh typo

--- codes/xspec-fitall.sh
+++ codes/xspec-fitall.sh
@@ -5,7 +5,7 @@ spexinit
 echo 'xspec parallel fitting begins!'
 python xspec-pipeline/bkg-atable-pipeline/main_bkg1st_para.py
 python xspec-pipeline/main_skybkg_para.py
-python xspec-pipeline/bkg-atable-pipeline/main_bkg2st_para.py
+python xspec-pipeline/bkg-atable-pipeline/main_bkg2nd_para.py
 python xspec-pipeline/main_icm_para.py
 
 echo 'xspec parallel fitting ends!'
