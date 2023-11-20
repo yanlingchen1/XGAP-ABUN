c061fc1e8634664b5cda6d77f48d315d0851ec18
Yanling Chen <cyl129@163.com>
2023-11-21 00:19:22 +0800

    create branch ltd

--- codes/xspec-fitall_loop.sh
+++ codes/xspec-fitall_loop.sh
@@ -5,7 +5,7 @@ spexinit
 echo 'xspec parallel fitting begins!'
 #python xspec-pipeline/bkg-atable-pipeline/main_bkg1st.py
 #python xspec-pipeline/main_skybkg.py
-python xspec-pipeline/bkg-atable-pipeline/main_bkg2nd.py
-#python xspec-pipeline/main_icm.py
+#python xspec-pipeline/bkg-atable-pipeline/main_bkg2nd.py
+python xspec-pipeline/main_icm.py
 
 echo 'xspec parallel fitting ends!'
