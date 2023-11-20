8a7483df8131bdbe7a96c7631e680007fd509c7c
Yanling Chen <cyl129@163.com>
2023-11-20 22:39:58 +0800

    io

--- codes/xspec-fitall_loop.sh
+++ codes/xspec-fitall_loop.sh
@@ -3,9 +3,9 @@ heainit
 conda activate xgap
 spexinit
 echo 'xspec parallel fitting begins!'
-python xspec-pipeline/bkg-atable-pipeline/main_bkg1st.py
+#python xspec-pipeline/bkg-atable-pipeline/main_bkg1st.py
 #python xspec-pipeline/main_skybkg.py
-#python xspec-pipeline/bkg-atable-pipeline/main_bkg2nd.py
+python xspec-pipeline/bkg-atable-pipeline/main_bkg2nd.py
 #python xspec-pipeline/main_icm.py
 
 echo 'xspec parallel fitting ends!'
