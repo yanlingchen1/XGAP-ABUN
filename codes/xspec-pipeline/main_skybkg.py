348b0081c53ced0b8e0e1b8aca532252e4cbba86
Yanling Chen <cyl129@163.com>
2023-11-20 15:41:41 +0100

    merge

--- codes/xspec-pipeline/main_skybkg.py
+++ codes/xspec-pipeline/main_skybkg.py
@@ -17,7 +17,7 @@ def main():
     basfile = f'/data/yanling/XGAP-ABUN/codes/XGAP-ABUN/codes/ESAS/get_nh/basics_allsources.csv'
     f = pd.read_csv(basfile)
 
-    for i, srcnum in enumerate(f['ID']): 
+    for i, srcnum in enumerate(f['ID'][0:2]): 
         srcnum = srcnum.split('G')[-1]
         nH = f['nH(1e20cm-2)'][i] * 1e-22
         reds = f['z'][i]
@@ -27,19 +27,11 @@ def main():
         if glob(f"/data/yanling/XGAP-ABUN/data/alldata/XGAP/{srcname2}"):
             root_dir = glob(f"/data/yanling/XGAP-ABUN/data/alldata/XGAP/{srcname2}")[0]
 
-
-            # # io issues
-            io_instance = IO(date, root_dir, srcname1, srcname2)
-            fit_other = FitOther(date, root_dir, srcname1, srcname2, REGNAME, nH, reds) 
-            fit_other.grp_spec()
-            fit_other.edit_headers()
-            io_instance.make_output_dir()
-            # io_instance.check_files()
-            
-            
             # # ## fit the sky bkg, after fit the qpb in bkgatablepiipeline
+            io_instance = IO(date, root_dir, srcname1, srcname2)
             fit_other = FitOther(date, root_dir, srcname1, srcname2, 'bkg', nH, reds)
             fit_other.grp_spec()
+            fit_other.edit_headers()
             fit_other.fit_oot()
             fit_other.fit_bkg()
             io_instance.tidy_bkgpar()
