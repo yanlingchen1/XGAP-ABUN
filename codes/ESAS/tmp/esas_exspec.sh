f58a62635d66a8f44902dfa20bd71b2ce210bf90
Yanling Chen <cyl129@163.com>
2023-11-20 03:32:26 +0100

    alter freezepar to freepar

--- codes/ESAS/tmp/esas_exspec.sh
+++ codes/ESAS/tmp/esas_exspec.sh
@@ -1,37 +1,37 @@
 
 #!/bin/bash
 heainit
-sasinit21
-source /data/yanling/XGAP-ABUN/data/data/ID828/reduction/SDSSTG828/set_chips_on.txt
-source /data/yanling/XGAP-ABUN/data/data/ID828/reduction/SDSSTG828/set_sas.txt
-#### MOS ####
-export M1=mos1S001
-export M2=mos2S002
+# sasinit21
+# source /data/yanling/XGAP-ABUN/data/data/ID828/reduction/SDSSTG828/set_chips_on.txt
+# source /data/yanling/XGAP-ABUN/data/data/ID828/reduction/SDSSTG828/set_sas.txt
+# #### MOS ####
+# export M1=mos1S001
+# export M2=mos2S002
 
-mkdir R500_01
-mkdir R500_01/R500_01
-mkdir R500_01/img
-mkdir R500_01/logs
-mkdir R500_01/diagnose
+# mkdir R500_01
+# mkdir R500_01/R500_01
+# mkdir R500_01/img
+# mkdir R500_01/logs
+# mkdir R500_01/diagnose
 
-# mosspectra eventfile=${M1}-allevc.fits keepinterfiles=yes withregion=yes regionfile=spec_reg/R500_01-${M1}.reg pattern=12 withsrcrem=yes maskdet=${M1}-bkgregtdet.fits masksky=${M1}-bkgregtsky.fits elow=700 ehigh=1200 ccds="${M1ON}" -V=7 2>&1 |tee R500_01/logs/mosspectra_${M1}.log 
-# mosback inspecfile=${M1}-fovt.pi elow=700 ehigh=1200 ccds="${M1ON}" 2>&1  |tee mosback_${M1}.log 
-mosspectra eventfile=${M2}-allevc.fits keepinterfiles=yes withregion=yes regionfile=spec_reg/R500_01-${M2}.reg pattern=12 withsrcrem=yes maskdet=${M2}-bkgregtdet.fits masksky=${M2}-bkgregtsky.fits elow=700 ehigh=1200 ccds="${M2ON}" -V=7 2>&1 |tee R500_01/logs/mosspectra_${M2}.log
-mosback inspecfile=${M2}-fovt.pi elow=700 ehigh=1200 ccds="${M2ON}" 2>&1  |tee mosback_${M2}.log 
+# # mosspectra eventfile=${M1}-allevc.fits keepinterfiles=yes withregion=yes regionfile=spec_reg/R500_01-${M1}.reg pattern=12 withsrcrem=yes maskdet=${M1}-bkgregtdet.fits masksky=${M1}-bkgregtsky.fits elow=700 ehigh=1200 ccds="${M1ON}" -V=7 2>&1 |tee R500_01/logs/mosspectra_${M1}.log 
+# # mosback inspecfile=${M1}-fovt.pi elow=700 ehigh=1200 ccds="${M1ON}" 2>&1  |tee mosback_${M1}.log 
+# mosspectra eventfile=${M2}-allevc.fits keepinterfiles=yes withregion=yes regionfile=spec_reg/R500_01-${M2}.reg pattern=12 withsrcrem=yes maskdet=${M2}-bkgregtdet.fits masksky=${M2}-bkgregtsky.fits elow=700 ehigh=1200 ccds="${M2ON}" -V=7 2>&1 |tee R500_01/logs/mosspectra_${M2}.log
+# mosback inspecfile=${M2}-fovt.pi elow=700 ehigh=1200 ccds="${M2ON}" 2>&1  |tee mosback_${M2}.log 
 
-for name in $M2
-do
-echo $name
+# for name in $M2
+# do
+# echo $name
 
-mv $name*-700-1200* R500_01/img
-mv $name-fovt.pi R500_01/R500_01/$name-obj-SDSSTG828_R500_01.pi
-mv $name-bkg.pi R500_01/R500_01/$name-back-SDSSTG828_R500_01.pi
-mv $name*.qdp R500_01/diagnose
-mv $name*imt* R500_01/img
-mv $name*.arf R500_01/R500_01/$name-SDSSTG828_R500_01.arf
-mv $name*.rmf R500_01/R500_01/$name-SDSSTG828_R500_01.rmf
-mv $name*imspdet* R500_01/img
-done
+# mv $name*-700-1200* R500_01/img
+# mv $name-fovt.pi R500_01/R500_01/$name-obj-SDSSTG828_R500_01.pi
+# mv $name-bkg.pi R500_01/R500_01/$name-back-SDSSTG828_R500_01.pi
+# mv $name*.qdp R500_01/diagnose
+# mv $name*imt* R500_01/img
+# mv $name*.arf R500_01/R500_01/$name-SDSSTG828_R500_01.arf
+# mv $name*.rmf R500_01/R500_01/$name-SDSSTG828_R500_01.rmf
+# mv $name*imspdet* R500_01/img
+# done
 
 #### PN ####
 heainit
@@ -55,9 +55,9 @@ mkdir R500_01/img/pn_$PAT
 mkdir R500_01/diagnose/pn_$PAT
 
 mv $name*-700-1200* R500_01/img/pn_$PAT
-mv $name-obj.pi R500_01/R500_01/$name-obj-$PAT-SDSSTG828_R500_01.pi
-mv $name-obj-oot.pi R500_01/R500_01/$name-obj-oot-$PAT-SDSSTG828_R500_01.pi
-mv $name-back.pi R500_01/R500_01/$name-back-$PAT-SDSSTG828_R500_01.pi
+mv $name-obj.pi R500_01/R500_01/$name-$PAT-obj-SDSSTG828_R500_01.pi
+mv $name-obj-oot.pi R500_01/R500_01/$name-$PAT-obj-oot-SDSSTG828_R500_01.pi
+mv $name-back.pi R500_01/R500_01/$name-$PAT-back-SDSSTG828_R500_01.pi
 mv $name*.qdp R500_01/diagnose/pn_$PAT
 mv $name*imt* R500_01/img/pn_$PAT
 mv $name*.arf R500_01/R500_01/$name-$PAT-SDSSTG828_R500_01.arf
